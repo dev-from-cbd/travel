72f2b02bba62bdcbf488f64f6e5ac3f6ce6c7db0
dev-from-cbd <veryfatwombat@gmail.com>
2023-10-23 06:46:58 +1030

    update

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -3,8 +3,11 @@ import Link from "next/link";
 
 const Navbar = () => {
   return (
-    <nav className="border-2 border-red-500 flexBetween max-container padding-container relative z-30 py-5">
-      <Link>Nav</Link>
+    //<nav className="flexBetween max-container padding-container relative z-30 py-5">
+    <nav className="flexBetween max-container relative z-30 py-5">
+      <Link href="/">
+        <Image src="/hilink-logo.svg" alt="logo" width={74} height={29} />
+      </Link>
     </nav>
   );
 };
