d7bee113820afea1cf9881b6cb06c85408d59c9d
dev-from-cbd <veryfatwombat@gmail.com>
2023-10-27 21:33:25 +1030

    comments

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,13 @@
+// Import components for the home page from their respective modules
 import Camp from "@/components/Camp";
 import Features from "@/components/Features";
 import GetApp from "@/components/GetApp";
 import Guide from "@/components/Guide";
 import Hero from "@/components/Hero";
 
+// Define the default function 'Home' for the home page
 export default function Home() {
+  // Return a fragment containing the components in the desired order
   return (
     <>
       <Hero />
